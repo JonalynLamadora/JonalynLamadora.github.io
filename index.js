3a8bedb4f8d136c8930a0f264f1444142df32ecf
JonalynLamadora <lamadorajonalyn15@gmail.com>
2024-05-25 18:30:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btnLike1 = document.getElementById("btnLike1")
 const btnLike2 = document.getElementById("btnLike2")
-
 function clickLike1(){
      let totalLikes = parseInt(countLike1.value) + 1
      countLike1.textContent = totalLikes.toString()
 }
+btnLike1.addEventListener("click",clickLike1)
